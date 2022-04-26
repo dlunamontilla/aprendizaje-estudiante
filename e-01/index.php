c1e2fcd65e1081929ad76c6685fac6683df70a7a
David E Luna M <davidlunamontilla@gmail.com>
2022-04-26 18:48:27 -0500

    Se actualizan algunos ejemplos

--- e-01/index.php
+++ e-01/index.php
@@ -1,14 +1,19 @@
+<?php
+    include __DIR__ . "/app.php";
+?>
+
 <!DOCTYPE html>
 <html lang="es-CO">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Primer ejemplo - Obtención de perfil</title>
 </head>
-<body>
-    <main id="app">
 
-    </main>
+<body>
+    <main id="app"></main>
 </body>
+
 </html>
